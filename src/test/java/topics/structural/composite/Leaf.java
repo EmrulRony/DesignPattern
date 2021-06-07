3d41ca1a615d0c90832aae09ff83569b32f338e7
Emrul Rony <emrul.hasan@bjitgroup.com>
2021-06-07 21:02:30 +0600

    composite design pattern

---
+++ src/test/java/topics/structural/composite/Leaf.java
@@ -0,0 +1,16 @@
+package topics.structural.composite_telescu;
+
+public class Leaf implements Component {
+    private String name;
+    private int price;
+
+    public Leaf(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public void showPrice() {
+        System.out.println("name: "+this.name + " price: "+this.price);
+    }
+}
